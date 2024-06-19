484744c8296b69b54e599c3e78690074ea2a1579
larrabeeja <larrabee.jason@gmail.com>
2024-06-19 12:23:59 -0400

    first commit

---
+++ week01/scripts/getdates.js/getdates.js
@@ -0,0 +1,5 @@
+// Dynamically populate the current year
+document.getElementById('currentyear').textContent = new Date().getFullYear();
+
+// Dynamically populate the last modified date
+document.getElementById('lastModified').textContent = 'Last Modified: ' + document.lastModified;
